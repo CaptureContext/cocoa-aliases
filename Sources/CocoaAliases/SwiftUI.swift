1e82baf5047e34730e3d49c9cbc81008e2b7fb50
maximkrouk <id.maximkrouk@gmail.com>
2026-03-04 16:16:47 +0100

    feat: SwiftUI CocoaRepresentable sizeThatFits

--- Sources/CocoaAliases/SwiftUI.swift
+++ Sources/CocoaAliases/SwiftUI.swift
@@ -29,21 +29,62 @@ public typealias CocoaHostingControllerSizingOptions = UIHostingControllerSizing
 public protocol CocoaViewRepresentable: UIViewRepresentable
 where UIViewType == CocoaViewType {
 	associatedtype CocoaViewType
-	func makeCocoaView(context: Context) -> CocoaViewType
-	func updateCocoaView(_ view: CocoaViewType, context: Context)
+	func makeCocoaView(
+		context: Context
+	) -> CocoaViewType
+	
+	func updateCocoaView(
+		_ view: CocoaViewType,
+		context: Context
+	)
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		cocoaView: CocoaViewType,
+		context: Context
+	) -> CGSize?
 }
 
 @available(iOS 13.0, tvOS 13.0, *)
 @available(macOS, unavailable)
 @available(watchOS, unavailable)
 extension CocoaViewRepresentable {
-	public func makeUIView(context: Context) -> CocoaViewType {
+	public func makeUIView(
+		context: Context
+	) -> CocoaViewType {
 		makeCocoaView(context: context)
 	}
 
-	public func updateUIView(_ view: CocoaViewType, context: Context) {
-		updateCocoaView(view, context: context)
+	public func updateUIView(
+		_ view: CocoaViewType,
+		context: Context
+	) {
+		updateCocoaView(
+			view,
+			context: context
+		)
 	}
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	public func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		uiView: CocoaViewType,
+		context: Context
+	) -> CGSize? {
+		sizeThatFits(
+			proposal,
+			cocoaView: uiView,
+			context: context
+		)
+	}
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		cocoaView: CocoaViewType,
+		context: Context
+	) -> CGSize? { nil }
 }
 
 @available(iOS 13.0, tvOS 13.0, *)
@@ -52,21 +93,62 @@ extension CocoaViewRepresentable {
 public protocol CocoaViewControllerRepresentable: UIViewControllerRepresentable
 where UIViewControllerType == CocoaViewControllerType {
 	associatedtype CocoaViewControllerType
-	func makeCocoaViewController(context: Context) -> CocoaViewControllerType
-	func updateCocoaViewController(_ controller: CocoaViewControllerType, context: Context)
+	func makeCocoaViewController(
+		context: Context
+	) -> CocoaViewControllerType
+
+	func updateCocoaViewController(
+		_ controller: CocoaViewControllerType,
+		context: Context
+	)
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		cocoaViewController: CocoaViewControllerType,
+		context: Context
+	) -> CGSize?
 }
 
 @available(iOS 13.0, tvOS 13.0, *)
 @available(macOS, unavailable)
 @available(watchOS, unavailable)
 extension CocoaViewControllerRepresentable {
-	public func makeUIViewController(context: Context) -> CocoaViewControllerType {
+	public func makeUIViewController(
+		context: Context
+	) -> CocoaViewControllerType {
 		makeCocoaViewController(context: context)
 	}
 
-	public func updateUIViewController(_ controller: CocoaViewControllerType, context: Context) {
-		updateCocoaViewController(controller, context: context)
+	public func updateUIViewController(
+		_ controller: CocoaViewControllerType,
+		context: Context
+	) {
+		updateCocoaViewController(
+			controller,
+			context: context
+		)
 	}
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		uiViewController: CocoaViewControllerType,
+		context: Context
+	) -> CGSize? {
+		sizeThatFits(
+			proposal,
+			cocoaViewController: uiViewController,
+			context: context
+		)
+	}
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		cocoaViewController: CocoaViewControllerType,
+		context: Context
+	) -> CGSize? { nil }
 }
 #endif
 
@@ -82,8 +164,21 @@ public typealias CocoaHostingController = NSHostingController
 public protocol CocoaViewRepresentable: NSViewRepresentable
 where NSViewType == CocoaViewType  {
 	associatedtype CocoaViewType
-	func makeCocoaView(context: Context) -> CocoaViewType
-	func updateCocoaView(_ view: CocoaViewType, context: Context)
+	func makeCocoaView(
+		context: Context
+	) -> CocoaViewType
+
+	func updateCocoaView(
+		_ view: CocoaViewType,
+		context: Context
+	)
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		cocoaView: CocoaViewType,
+		context: Context
+	) -> CGSize?
 }
 
 @available(macOS 10.15, *)
@@ -91,13 +186,41 @@ where NSViewType == CocoaViewType  {
 @available(tvOS, unavailable)
 @available(watchOS, unavailable)
 extension CocoaViewRepresentable {
-	public func makeNSView(context: Context) -> CocoaViewType {
+	public func makeNSView(
+		context: Context
+	) -> CocoaViewType {
 		makeCocoaView(context: context)
 	}
 
-	public func updateNSView(_ view: CocoaViewType, context: Context) {
-		updateCocoaView(view, context: context)
+	public func updateNSView(
+		_ view: CocoaViewType,
+		context: Context
+	) {
+		updateCocoaView(
+			view,
+			context: context
+		)
 	}
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	public func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		nsView: CocoaViewType,
+		context: Context
+	) -> CGSize? {
+		sizeThatFits(
+			proposal,
+			cocoaView: nsView,
+			context: context
+		)
+	}
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		cocoaView: CocoaViewType,
+		context: Context
+	) -> CGSize? { nil }
 }
 
 @available(macOS 10.15, *)
@@ -107,8 +230,21 @@ extension CocoaViewRepresentable {
 public protocol CocoaViewControllerRepresentable: NSViewControllerRepresentable
 where NSViewControllerType == CocoaViewControllerType {
 	associatedtype CocoaViewControllerType
-	func makeCocoaViewController(context: Context) -> CocoaViewControllerType
-	func updateCocoaViewController(_ controller: CocoaViewControllerType, context: Context)
+	func makeCocoaViewController(
+		context: Context
+	) -> CocoaViewControllerType
+
+	func updateCocoaViewController(
+		_ controller: CocoaViewControllerType,
+		context: Context
+	)
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		cocoaViewController: CocoaViewControllerType,
+		context: Context
+	) -> CGSize?
 }
 
 @available(macOS 10.15, *)
@@ -123,6 +259,26 @@ extension CocoaViewControllerRepresentable {
 	public func updateNSViewController(_ controller: CocoaViewControllerType, context: Context) {
 		updateCocoaViewController(controller, context: context)
 	}
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	public func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		nsViewController: CocoaViewControllerType,
+		context: Context
+	) -> CGSize? {
+		sizeThatFits(
+			proposal,
+			cocoaViewController: nsViewController,
+			context: context
+		)
+	}
+
+	@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+	func sizeThatFits(
+		_ proposal: ProposedViewSize,
+		cocoaViewController: CocoaViewControllerType,
+		context: Context
+	) -> CGSize? { nil }
 }
 #endif
 #endif
