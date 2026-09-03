19cc32ff3cb07e7c30e53d25bf707ac29b1ffd1e
maximkrouk <id.maximkrouk@gmail.com>
2026-09-03 14:27:34 +0200

    chore: update marker protocols dependency

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -20,7 +20,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/capturecontext/swift-marker-protocols.git",
-			.upToNextMajor(from: "1.5.0")
+			.upToNextMajor(from: "1.5.3")
 		),
 	],
 	targets: [
